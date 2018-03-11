2becfffe1233542433acc86d2d41a01abe940306
twinxevo <37262601+twinxevo@users.noreply.github.com>
2018-03-11 09:31:58 +0000

    Add files via upload

---
+++ data printer.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+	printf("this is a program to input your name,age,phone number and department\n");
+	
+	printf("please input first name:\n");
+	scanf("%d,\n");
+
+	printf("please input last name:\n");
+	scanf("%d,\n");
+	
+int n,array[100],i;
+	printf("please input age in the order mm/dd/yy:\n");
+    scanf("%d",&n);
+  
+    printf("please input your phone number:\n");
+    scanf("%d",&n);
+    
+    printf("please input your department:\n");
+    scanf("%d,\n");
+    
+    printf("please input your Reg/Mat number correctly:\n");
+    scanf("%d",&n);
+    
+    printf("Please make sure you entered your data correctly\n Thank You.");
+	return (0);
+}
